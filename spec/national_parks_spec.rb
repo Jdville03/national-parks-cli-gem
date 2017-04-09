e00d55b297b33cf3beaf0394cd1195585ee511e5
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-04-09 03:40:07 -0400

    first commit

---
+++ spec/national_parks_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe NationalParks do
+  it "has a version number" do
+    expect(NationalParks::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
