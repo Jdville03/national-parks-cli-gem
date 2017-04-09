e9182df9c2fa989a48f60d8790c3e7089621adc9
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-04-09 18:59:16 -0400

    Update park.rb

--- lib/national_parks/park.rb
+++ lib/national_parks/park.rb
@@ -1,16 +1,16 @@
 class NationalParks::Park
 
-	attr_accessor :state, :type, :name, :location, :description, :info_url
+  attr_accessor :state, :type, :name, :location, :description, :info_url
 
-	def initialize(attribute_hash = nil)
-		if attribute_hash
-			attribute_hash.each{|key, value| self.send("#{key}=", value)}
-		end
-	end
+  def initialize(attribute_hash = nil)
+    if attribute_hash
+      attribute_hash.each{|key, value| self.send("#{key}=", value)}
+    end
+  end
 
-	# optionality to add more park attributes if desired
+  # optionality to add more park attributes if desired
 	
-	#def add_park_attributes(attributes_hash)
+  #def add_park_attributes(attributes_hash)
   #  attributes_hash.each{|key, value| self.send("#{key}=", value)}
   #  self
   #end
