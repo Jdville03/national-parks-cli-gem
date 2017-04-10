3b564bbe1c91775f264797a8171d661cb604f9e2
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-04-10 05:51:10 -0400

    Add colorize dependency

--- lib/national_parks.rb
+++ lib/national_parks.rb
@@ -4,6 +4,7 @@ require "open-uri"
 require "nokogiri"
 require "pry"
 require "require_all"
+require "colorize"
 
 require_relative "./national_parks/version"
 require_relative "./national_parks/cli"
