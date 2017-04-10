a09160ba02206716e8e04376b14dc2e1969f3e97
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-04-10 05:02:51 -0400

    Update CLI

--- lib/national_parks/cli.rb
+++ lib/national_parks/cli.rb
@@ -1,7 +1,11 @@
 class NationalParks::CLI
 	
-  def call
+  def initialize
   	welcome
+  	NationalParks::Scraper.scrape_find_park_page
+  end
+
+  def call
     list_states
     state_selection
     list_parks
@@ -18,7 +22,6 @@ class NationalParks::CLI
   def list_states
   	puts "\nFind information about the national parks located in any of the U.S. states or territories:"
   	puts ""
-  	NationalParks::Scraper.scrape_find_park_page
   	NationalParks::State.all.each.with_index(1) do |state, index|
   		puts "#{index}. #{state.name}"
   	end
@@ -27,40 +30,43 @@ class NationalParks::CLI
   def state_selection
   	puts "\nSelect a state or territory by number or name:"
   	state_input = gets.strip
-  	if state_input.to_i.between?(1..NationalParks::State.all.length)
-  		state = NationalParks::State.find_state(state_input)
+  	if state_input.to_i.between?(1, NationalParks::State.all.length)
+  		@state = NationalParks::State.find_state(state_input)
   	elsif NationalParks::State.find_state_by_name(state_input)
-  		state = NationalParks::State.find_state_by_name(state_input)
+  		@state = NationalParks::State.find_state_by_name(state_input)
   	else
   		puts "Invalid entry."
   		state_selection
+  	end
   end
 
   def list_parks
-  	system("clear")	
-  	NationalParks::Scraper.scrape_state_page(state_selection)
-  	state_selection.parks.each.with_index(1) do |park, index|
+  	NationalParks::Scraper.scrape_state_page(@state)
+  	system("clear")
+  	sleep(0.25)
+  	puts "\nNational Parks in #{@state.name}:"
+  	@state.parks.each.with_index(1) do |park, index|
   		puts "\n-------------------------------------"
   		puts "#{index}. #{park.name}"
   		puts ""
   		puts "Type:         #{park.type}" if park.type
   		puts "Location:     #{park.location}" if park.location
   		puts "Description:  #{park.description}"
+  		sleep(0.25)
   	end
-  	
+
   	input = nil
-  	while != "exit"
-  		puts "\nIf you would like to see more information for one of these parks in your web browser, enter the number of the park. Type \"list\" to start over and see the states and territories again. Or type \"exit\"."
-			park_input = gets.strip
-			if park_input.to_i.between?(1..state_selection.parks.length)
-				system("open #{state.find_park(park_input).more_info_url}")
-			elsif park_input.downcase == "list"
+  	while input != "exit"
+  		puts "\nTo see more information in your web browser for a national park in #{@state.name}, enter the number of the park. Type \"list\" to start over and see the states and territories again. Or type \"exit\"."
+			input = gets.strip
+			if input.to_i.between?(1, @state.parks.length)
+				system("open #{@state.find_park(park_input).more_info_url}")
+			elsif input.downcase == "list"
 				call
-			elsif park_input.downcase == "exit"
+			elsif input.downcase == "exit"
 				puts "Get out and enjoy the national parks! Goodbye!"
 			else
-				puts "Invalid entry. Try again."
-				park_input = gets.strip
+				puts "Invalid entry."
 			end	
 		end
   end
